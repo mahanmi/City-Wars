46c3f1afec09cb197bf37c42635df03ea096b779
Mahan MI <majlesi.mahan@gmail.com>
2024-07-02 09:52:49 +0330

    Added Static User to Main

--- city-wars/src/main/java/com/Main.java
+++ city-wars/src/main/java/com/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static final int INITIAL_BALANCE = 1000;
     public static int loggedInUserId = -1;
     public static CRUD crud = new CRUD();
+    public static User loggedInUser = null;
+    public static String input;
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
     }
+
+    
+
 }
