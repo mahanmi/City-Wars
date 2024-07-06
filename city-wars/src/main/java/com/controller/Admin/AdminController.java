b9920c1f4657a2c7d5f3d19c6dc268d8c842431d
Mahan MI(PC) <majlesi.mahan@gmail.com>
2024-07-06 07:37:26 +0330

    refactor: Improve admin menu readability and formatting

--- city-wars/src/main/java/com/controller/Admin/AdminController.java
+++ city-wars/src/main/java/com/controller/Admin/AdminController.java
@@ -8,29 +8,29 @@ import com.model.*;
 public class AdminController {
     final String password = "admin";
 
-    public void login(String password, Scanner scanner){
+    public void login(String password, Scanner scanner) {
         boolean validPassword = false;
-        while(!validPassword){
-            if (password.equals(this.password)){
+        while (!validPassword) {
+            if (password.equals(this.password)) {
                 System.out.println("Logged in as admin.");
                 validPassword = true;
                 adminMenu(scanner);
-            }
-            else{
+            } else {
                 System.out.println("Incorrect password.");
             }
         }
     }
-    public void adminMenu(Scanner scanner){
-        while(!Main.input.equals("back")){
+
+    public void adminMenu(Scanner scanner) {
+        while (!Main.input.equals("back")) {
             System.out.println("What would you like to do?");
             System.out.println("1. Add a card");
             System.out.println("2. Edit a card");
             System.out.println("3. Delete a card");
             System.out.println("4. View all players");
-            
+
             Main.input = scanner.nextLine();
-            switch (Main.input){
+            switch (Main.input) {
                 case "1":
                     addCard(scanner);
                     break;
@@ -52,7 +52,7 @@ public class AdminController {
         }
     }
 
-    public void addCard(Scanner scanner){
+    public void addCard(Scanner scanner) {
         System.out.println("Enter the name of the card:");
         String name = scanner.nextLine();
         System.out.println("Enter the power of the card:");
@@ -72,21 +72,20 @@ public class AdminController {
         System.out.println("Card added successfully.");
     }
 
-    public void editCard(Scanner scanner){
-        ArrayList<Card> cards ; //mahan por kon
-        cards = Main.crud.getAllCards();
-        for (Card i : cards){
+    public void editCard(Scanner scanner) {
+        ArrayList<Card> cards = Main.crud.getAllCards();
+        for (Card i : cards) {
             System.out.println(i.getId() + ". " + i.getName());
         }
         System.out.println("Enter the id of the card you want to edit:");
         Main.input = scanner.nextLine();
-        if(Main.input.equals("back")){
+        if (Main.input.equals("back")) {
             return;
         }
         int id = Integer.parseInt(Main.input);
         Card card = Main.crud.getCard(id);
         System.out.println(card.getCardInfo());
-        
+
         System.out.println("Enter card's new name:");
         String name = scanner.nextLine();
         System.out.println("Enter card's new power:");
@@ -100,40 +99,37 @@ public class AdminController {
         System.out.println("Enter card's new upgrade cost:");
         int upgradeCost = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Are you sure you want to edit this card? (y/n)");
+        System.out.println("Are you sure you want to edit this card? (y/n)[default: n]");
         Main.input = scanner.nextLine();
-        if(Main.input.equals("y")){
+        if (Main.input.equals("y")) {
             Card newCard = new Card(name, power, duration, damage, upgradeLevel, upgradeCost);
             Main.crud.updateCard(newCard, id);
             System.out.println("Card edited successfully.");
-        }
-        else{
+        } else {
             return;
         }
     }
 
-    public void deleteCard(Scanner scanner){
-        ArrayList<Card> cards ; //mahan por kon
-        cards = Main.crud.getAllCards();
-        for (Card i : cards){
+    public void deleteCard(Scanner scanner) {
+        ArrayList<Card> cards = Main.crud.getAllCards();
+        for (Card i : cards) {
             System.out.println(i.getId() + ". " + i.getName());
         }
         System.out.println("Enter the id of the card you want to delete:");
         Main.input = scanner.nextLine();
-        if(Main.input.equals("back")){
+        if (Main.input.equals("back")) {
             return;
         }
         int id = Integer.parseInt(scanner.nextLine());
         Card card = Main.crud.getCard(id);
         System.out.println(card.getCardInfo());
-        
-        System.out.println("Are you sure you want to delete this card? (y/n)");
+
+        System.out.println("Are you sure you want to delete this card? (y/n)[default: n]");
         Main.input = scanner.nextLine();
-        if(Main.input.equals("y")){
+        if (Main.input.equals("y")) {
             Main.crud.deleteCard(id);
             System.out.println("Card deleted successfully.");
-        }
-        else{
+        } else {
             return;
         }
     }
